0c870bf00fafc31b9b61d674d603fe4f5d3727cd
toinhao1 <41380451+toinhao1@users.noreply.github.com>
2020-07-15 15:05:17 +0200

    Added CandidateBio functionality to package

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const setApiKey = require('./lib/setApiKey');
 const Address = require('./lib/Address/index');
 const BallotMeasure = require('./lib/Ballot-Measure/index');
+const CandidateBio = require('./lib/CandidateBio/index');
 
 module.exports = voteSmart = {
 	setApiKey,
 	Address,
 	BallotMeasure,
+	CandidateBio,
 };
