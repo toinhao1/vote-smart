c27ea56e14387d8774d21c6daf5e7cde46b7383d
toinhao1 <41380451+toinhao1@users.noreply.github.com>
2020-07-15 14:19:08 +0200

    Address functions finished

---
+++ index.js
@@ -0,0 +1,11 @@
+var setApiKey = require('./lib/setApiKey');
+var Address = require('./lib/Address/index');
+
+module.exports = voteSmart = {
+	setApiKey,
+	Address,
+};
+
+setApiKey('kjfododshfojweojffdf');
+
+// console.log(global.voteSmartApiKey);
