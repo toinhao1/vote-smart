46d5cbbde3aa02115485fd2b92a5c1a145b715be
toinhao1 <41380451+toinhao1@users.noreply.github.com>
2020-07-15 23:45:35 +0200

    Added Committee methods

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const Address = require('./lib/Address/index');
 const BallotMeasure = require('./lib/Ballot-Measure/index');
 const CandidateBio = require('./lib/CandidateBio/index');
 const Candidates = require('./lib/Candidates/index');
+const Committee = require('./lib/Committee/index');
 
 module.exports = voteSmart = {
 	setApiKey,
@@ -10,4 +11,5 @@ module.exports = voteSmart = {
 	BallotMeasure,
 	CandidateBio,
 	Candidates,
+	Committee,
 };
