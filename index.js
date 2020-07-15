82ab16d202d075113f98dbfe20d904c13028c5c6
toinhao1 <41380451+toinhao1@users.noreply.github.com>
2020-07-15 22:18:42 +0200

    Added all candidates methods

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const setApiKey = require('./lib/setApiKey');
 const Address = require('./lib/Address/index');
 const BallotMeasure = require('./lib/Ballot-Measure/index');
 const CandidateBio = require('./lib/CandidateBio/index');
+const Candidates = require('./lib/Candidates/index');
 
 module.exports = voteSmart = {
 	setApiKey,
 	Address,
 	BallotMeasure,
 	CandidateBio,
+	Candidates,
 };
