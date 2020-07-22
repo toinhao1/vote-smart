caf29b46649f5cb2d7e3fe9036c6da3cefdaf76d
toinhao1 <41380451+toinhao1@users.noreply.github.com>
2020-07-22 18:38:37 +0200

    all tests for the Address method set up

--- test/address.test.js
+++ test/address.test.js
@@ -1,11 +1,10 @@
 const Address = require('../lib/Address');
 const setApiKey = require('../lib/setApiKey');
 var expect = require('chai').expect;
-var assert = require('chai').assert;
 
-// Joe bidens candidateId 53279
-
-var joeBidensId = 53279;
+var candidId = 15723;
+var electionId = 3916;
+var officeId = 1;
 
 describe('tests all methods from the address class', function () {
 	before(function () {
@@ -13,20 +12,80 @@ describe('tests all methods from the address class', function () {
 	});
 	describe('getCampaign method', function () {
 		it('returns data when providing correct params to getCampaign', async function () {
-			const response = await Address.getCampaign(joeBidensId);
+			const response = await Address.getCampaign(candidId);
 			expect(response.address.office).length.to.be.greaterThan(0);
 		});
-		// it('throws an error when no input', async function () {
-		// 	assert.isRejected(Address.getCampaign(), Error);
-		// });
+		it('throws an error when no input is provided', async function () {
+			try {
+				await Address.getCampaign();
+			} catch (err) {
+				expect(err).to.not.be.null;
+			}
+		});
 	});
 	describe('getCampaignWebAddress method', function () {
-		it('returns data when providing correct params to getCampaign', async function () {
-			const response = await Address.getCampaignWebAddress(joeBidensId);
-			expect(response.address).length.to.be.greaterThan(0);
+		it('returns data when providing correct params to getCampaignWebAddress', async function () {
+			const response = await Address.getCampaignWebAddress(candidId);
+			expect(response.webaddress.address).length.to.be.greaterThan(0);
 		});
-		// it('throws an error when no input', async function () {
-		// 	assert.isRejected(Address.getCampaign(), Error);
-		// });
+		it('throws an error when no input is provided', async function () {
+			try {
+				await Address.getCampaignWebAddress();
+			} catch (err) {
+				expect(err).to.not.be.null;
+			}
+		});
+	});
+	describe('getCampaignByElection method', function () {
+		it('returns data when providing correct params to getCampaignByElection', async function () {
+			const response = await Address.getCampaignByElection(electionId);
+			expect(response.address.office).length.to.be.greaterThan(0);
+		});
+		it('throws an error when no input is provided', async function () {
+			try {
+				await Address.getCampaignByElection();
+			} catch (err) {
+				expect(err).to.not.be.null;
+			}
+		});
+	});
+	describe('getOffice method', function () {
+		it('returns data when providing correct param to getOffice', async function () {
+			const response = await Address.getOffice(candidId);
+			expect(response.address.office.address).to.haveOwnProperty('street');
+		});
+		it('throws an error when no input is provided', async function () {
+			try {
+				await Address.getOffice();
+			} catch (err) {
+				expect(err).to.not.be.null;
+			}
+		});
+	});
+	describe('getOfficeWebAddress method', function () {
+		it('returns data when providing correct param to getOfficeWebAddress', async function () {
+			const response = await Address.getOfficeWebAddress(candidId);
+			expect(response.webaddress.address).length.to.be.greaterThan(0);
+		});
+		it('throws an error when no input is provided', async function () {
+			try {
+				await Address.getOfficeWebAddress();
+			} catch (err) {
+				expect(err).to.not.be.null;
+			}
+		});
+	});
+	describe('getOfficeByOfficeState method', function () {
+		it('returns data when providing correct param to getOfficeByOfficeState', async function () {
+			const response = await Address.getOfficeByOfficeState(officeId);
+			expect(response.address.office.address).to.haveOwnProperty('street');
+		});
+		it('throws an error when no input is provided', async function () {
+			try {
+				await Address.getOfficeByOfficeState();
+			} catch (err) {
+				expect(err).to.not.be.null;
+			}
+		});
 	});
 });
