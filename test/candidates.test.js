4ffb54718a0aa5bd7d211d078bdf4f409af752df
toinhao1 <41380451+toinhao1@users.noreply.github.com>
2020-07-23 15:08:01 +0200

    Finished candidates methods

--- test/candidates.test.js
+++ test/candidates.test.js
@@ -8,6 +8,10 @@ const {
 	officeTypeId,
 	candidateLastName,
 	electionId,
+	stageId,
+	districtId,
+	zip5,
+	zip4,
 } = require('./data');
 
 describe('all methods for the Candidates class', function () {
@@ -15,14 +19,14 @@ describe('all methods for the Candidates class', function () {
 		setApiKey('APIKEY');
 	});
 	describe('getByOfficeState method', function () {
-		it('should return a valid array of canddiates when providing correct params', async function () {
+		it('should return a valid array of canddiates when providing correct params.', async function () {
 			const {
 				candidateList: { candidate },
 			} = await Candidates.getByOfficeState(officeId, stateId, year);
 			expect(candidate).to.not.be.null;
 			expect(candidate).to.be.an('array');
 		});
-		it('should return a valid array of canddiates when providing only required param', async function () {
+		it('should return a valid array of canddiates when providing only required param.', async function () {
 			const {
 				candidateList: { candidate },
 			} = await Candidates.getByOfficeState(officeId);
@@ -38,14 +42,14 @@ describe('all methods for the Candidates class', function () {
 		});
 	});
 	describe('getByOfficeTypeState method', function () {
-		it('should return a valid array of canddiates when providing correct params', async function () {
+		it('should return a valid array of canddiates when providing correct params.', async function () {
 			const {
 				candidateList: { candidate },
 			} = await Candidates.getByOfficeTypeState(officeTypeId, stateId, year);
 			expect(candidate).to.not.be.null;
 			expect(candidate).to.be.an('array');
 		});
-		it('should return a valid array of canddiates when providing only required param', async function () {
+		it('should return a valid array of canddiates when providing only required param.', async function () {
 			const {
 				candidateList: { candidate },
 			} = await Candidates.getByOfficeTypeState(officeTypeId);
@@ -68,14 +72,14 @@ describe('all methods for the Candidates class', function () {
 		});
 	});
 	describe('getByLastname method', function () {
-		it('should return a valid array of canddiates when providing correct params', async function () {
+		it('should return a valid array of canddiates when providing correct params.', async function () {
 			const {
 				candidateList: { candidate },
 			} = await Candidates.getByLastname(candidateLastName, year);
 			expect(candidate).to.not.be.null;
 			expect(candidate).to.be.an('array');
 		});
-		it('should return a valid array of canddiates when providing only required param', async function () {
+		it('should return a valid array of canddiates when providing only required param.', async function () {
 			const {
 				candidateList: { candidate },
 			} = await Candidates.getByLastname(candidateLastName);
@@ -98,7 +102,7 @@ describe('all methods for the Candidates class', function () {
 		});
 	});
 	describe('getByLevenshtein methods', function () {
-		it('should return an array of candidates close to the name provided', async function () {
+		it('should return an array of candidates close to the name provided.', async function () {
 			const {
 				candidateList: { candidate },
 			} = await Candidates.getByLevenshtein(candidateLastName, year);
@@ -131,11 +135,93 @@ describe('all methods for the Candidates class', function () {
 		});
 	});
 	describe('getByElection method', function () {
-		it('should return an array of candidates for a given election', async function () {
-			const res = await Candidates.getByElection(electionId);
-			console.log(res);
-			// expect(candidate).to.not.be.null;
-			// expect(candidate).to.be.an('array');
+		it('should return an array of candidates with both params.', async function () {
+			const {
+				candidateList: { candidate },
+			} = await Candidates.getByElection(electionId, stageId);
+			expect(candidate).to.not.be.null;
+			expect(candidate).to.be.an('array');
+		});
+		it('should return an array of candidates with required param.', async function () {
+			const {
+				candidateList: { candidate },
+			} = await Candidates.getByElection(electionId);
+			expect(candidate).to.not.be.null;
+			expect(candidate).to.be.an('array');
+		});
+		it('should throw an error with no params.', async function () {
+			try {
+				await Candidates.getByElection();
+			} catch (err) {
+				expect(err).to.not.be.null;
+			}
+		});
+		it('should throw an error with optional param.', async function () {
+			try {
+				await Candidates.getByElection('', stageId);
+			} catch (err) {
+				expect(err).to.not.be.null;
+			}
+		});
+	});
+	describe('getByDistrict method', function () {
+		it('should retun an array with candidates', async function () {
+			const {
+				candidateList: { candidate },
+			} = await Candidates.getByDistrict(districtId, year, stageId);
+			expect(candidate).to.not.be.null;
+			expect(candidate).to.be.an('array');
+		});
+		it('should retun an array with candidates, only required param', async function () {
+			const {
+				candidateList: { candidate },
+			} = await Candidates.getByDistrict(districtId);
+			expect(candidate).to.not.be.null;
+			expect(candidate).to.be.an('array');
+		});
+		it('should throw an error without required param', async function () {
+			try {
+				await Candidates.getByDistrict('', year, stageId);
+			} catch (err) {
+				expect(err).to.not.be.null;
+			}
+		});
+		it('should throw an error without any params', async function () {
+			try {
+				await Candidates.getByDistrict();
+			} catch (err) {
+				expect(err).to.not.be.null;
+			}
+		});
+	});
+	describe('getByZip method', function () {
+		it('should return an array of candidates with all params.', async function () {
+			const {
+				candidateList: { candidate },
+			} = await Candidates.getByZip(zip5, year, zip4, stageId);
+			expect(candidate).to.not.be.null;
+			expect(candidate).to.be.an('array');
+		});
+		it('should return an array of candidates with just zip5.', async function () {
+			const {
+				candidateList: { candidate },
+			} = await Candidates.getByZip(zip5);
+			expect(candidate).to.not.be.null;
+			expect(candidate).to.be.an('array');
+		});
+		it('should throw an error without zip5.', async function () {
+			try {
+				await Candidates.getByZip(null, year, zip4);
+			} catch (err) {
+				expect(err).to.not.be.null;
+			}
+		});
+		it('should throw an error without params.', async function () {
+			try {
+				await Candidates.getByZip();
+			} catch (err) {
+				expect(err).to.not.be.null;
+			}
 		});
 	});
 });
